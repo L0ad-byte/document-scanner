af1d282f38668a7fbfd222385dda20816fad6cad
L0ad-byte <jarnodutoit1@outlook.com>
2024-11-25 13:11:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const documentTypeSelect = document.getElementById('documentType');
 let capturedImageData = '';
 
 // Google Apps Script Web App URL
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxPh9qWECof8zG8lGAdOwnNlXqgO-9rKzvEo0GOtElRsuOPJ4lAY_fTfJjB6HcGmUKu/exec'; // TODO: Replace with your Web App URL
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxcuBdmkcQfk1qJeZKPD6njeSJ7w23aO_YUUlQB8lQkYHS7au7EeFC8EIs_QS-Ln2e8/exec'; // TODO: Replace with your Web App URL
 
 // Logging variables
 let logs = []; // Array to store log messages
